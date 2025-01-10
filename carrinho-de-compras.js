60d520f00911eda6e71038d29abb9cb22270b1a4
fbarceloss <161656179+fbarceloss@users.noreply.github.com>
2025-01-10 14:39:39 +0000

    Adicioan função calcularTotal.

--- carrinho-de-compras.js
+++ carrinho-de-compras.js
@@ -5,13 +5,28 @@ function adicionarProduto(nome, preco, quantidade) {
         preco:preco,
         quantidade:quantidade
     }
-
     return produto
-
 }
-
 const produto1 = adicionarProduto('Camiseta', 50.00, 2)
 const produto2 = adicionarProduto('Bermuda', 35.00, 2)
 const produto3 = adicionarProduto('Meia', 10.00, 4)
 const carrinho = [produto1, produto2, produto3]
-console.log(carrinho)
+
+function calcularTotal() {
+
+    let valor_total = 0
+
+    for (let prod in carrinho) {
+        
+    const produto_separado = carrinho[prod]
+    valor_total += produto_separado.preco * produto_separado.quantidade // Utilize o += para adicionar a soma a variavel
+        
+    }
+    
+
+    // Retorne a variavel valor_total no console.log
+    return console.log(valor_total)
+
+}
+
+calcularTotal()
