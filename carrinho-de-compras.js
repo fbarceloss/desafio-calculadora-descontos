df77ce3ad547cccbe7b02ad9db8c369e09081ee4
fbarceloss <161656179+fbarceloss@users.noreply.github.com>
2025-01-10 15:12:22 +0000

    INSTRUÇÕES DO CÓDIGO

--- carrinho-de-compras.js
+++ carrinho-de-compras.js
@@ -1,32 +1,33 @@
 function adicionarProduto(nome, preco, quantidade) {
-
+//função adiciona atributos do produto (definidos argumentos)
     const produto = {
         nome:nome,
         preco:preco,
         quantidade:quantidade
     }
-    return produto
+    return produto //retorna produto com as variaveis definidas pelas atribuições da função
 }
 const produto1 = adicionarProduto('Camiseta', 50.00, 2)
 const produto2 = adicionarProduto('Bermuda', 35.00, 2)
 const produto3 = adicionarProduto('Meia', 10.00, 4)
 const carrinho = [produto1, produto2, produto3]
+//criação das variaveis produtos (prod1/2/3) e do carrinho em array (composto pelos produtos)
 
 function calcularTotal() {
 
     let valor_total = 0
 
     for (let prod in carrinho) {
-        
+
     const produto_separado = carrinho[prod]
-    valor_total += produto_separado.preco * produto_separado.quantidade // Utilize o += para adicionar a soma a variavel
-        
-    }
     
-
-    // Retorne a variavel valor_total no console.log
+    valor_total += produto_separado.preco * produto_separado.quantidade
+    }
+        //função inicia o valor total em 0 (let para que possa ser definido mais tarde como soma (+=))
+        //for percorre o carrinho e identifica todos as variaveis produtos definidas
+        //variavel produto_separado trás o conteúdo das variaveis produto no carrinho após o for trazer os produtos separadamente, resultando em separação da array por item.
+        //valor total definido agora como soma de PREÇO * QTD (total = SUM(x * y) )
     return console.log(valor_total)
-
-}
+} //retorna o valor total calculado
 
 calcularTotal()
--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 function calcularPrecoFinal(preco, porcentagem) {
-
+//defini a função com os argumentos que serão calculados
     const desconto = (preco * porcentagem) / 100
     const precoFinal = preco - desconto
-
+//variaveis com fórmula de calculo (valores a serem definidos)
     return precoFinal
-
+//trás o preço final
 }
 
 const preco = 100
 const porcentagem = 25
+//definição de valores para os argumentos definidos na função
 const precoFinal = calcularPrecoFinal(preco,porcentagem)
+//chamei a função pra calcular pra calcular a variavel (precoFinal)
 console.log(precoFinal)
+//retorna precoFinal (cálculo feito - valor final)
